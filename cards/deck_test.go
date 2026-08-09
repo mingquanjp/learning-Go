f130536a72b8486a49bc3fedebb3b6235a0f450e
ming quan <faker1652005@gmail.com>
2026-08-09 21:31:03 +0700

    Add tests for newDeck and file operations in deck_test.go

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 20 {
+		t. Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if (d[0] != "Ace of Spades") {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if (d[len(d)-1] != "Five of Clubs") {
+		t.Errorf("Expected last card to be Five of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.RemoveAll("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if (len(loadedDeck) !=20) {
+		t.Errorf("Expected deck length of 20, but got %v", len(loadedDeck))
+	}
+
+	os.RemoveAll("_decktesting")
+}
