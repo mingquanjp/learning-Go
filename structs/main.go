e1d30c531ac1e65d7a4aff62f4c03c5717b57218
ming quan <faker1652005@gmail.com>
2026-08-10 09:26:22 +0700

    Add person struct with methods for printing and updating name

---
+++ structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+func main() {
+	quan := person{
+		firstName : "Quan",
+		lastName : "Nguyen",
+		contact : contactInfo{
+			email : "quan@gmail.com",
+			zipCode : 12345,
+		},
+	}
+	quan.print()
+	quan.updateName("John")
+	quan.print()
+
+	
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
